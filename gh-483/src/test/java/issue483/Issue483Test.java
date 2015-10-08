d6dc759619e9ca02d2e56e25bea9deac1a4c6b0f
Iwao AVE! <harawata@gmail.com>
2015-10-09 03:34:17 +0900

    Corrected format.

--- gh-483/src/test/java/issue483/Issue483Test.java
+++ gh-483/src/test/java/issue483/Issue483Test.java
@@ -25,95 +25,81 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class Issue483Test
-{
+public class Issue483Test {
 
-	private static SqlSessionFactory sqlSessionFactory;
+  private static SqlSessionFactory sqlSessionFactory;
 
-	@BeforeClass
-	public static void setUp() throws Exception
-	{
-		// create an SqlSessionFactory
-		Reader reader = Resources.getResourceAsReader("issue483/mybatis-config.xml");
-		sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-		reader.close();
+  @BeforeClass
+  public static void setUp() throws Exception {
+    // create an SqlSessionFactory
+    Reader reader = Resources.getResourceAsReader("issue483/mybatis-config.xml");
+    sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+    reader.close();
 
-		// populate in-memory database
-		SqlSession session = sqlSessionFactory.openSession();
-		Connection conn = session.getConnection();
-		reader = Resources.getResourceAsReader("issue483/CreateDB.sql");
-		ScriptRunner runner = new ScriptRunner(conn);
-		runner.setLogWriter(null);
-		runner.runScript(reader);
-		reader.close();
-		session.close();
-	}
+    // populate in-memory database
+    SqlSession session = sqlSessionFactory.openSession();
+    Connection conn = session.getConnection();
+    reader = Resources.getResourceAsReader("issue483/CreateDB.sql");
+    ScriptRunner runner = new ScriptRunner(conn);
+    runner.setLogWriter(null);
+    runner.runScript(reader);
+    reader.close();
+    session.close();
+  }
 
-	@Test
-	public void testSaveOrderDetail_1()
-	{
-		SqlSession sqlSession = sqlSessionFactory.openSession();
-		try
-		{
-			OrderDao mapper = sqlSession.getMapper(OrderDao.class);
-			List<OrderDetail> details = new ArrayList<OrderDetail>();
-			OrderDetail detail = new OrderDetail();
-			detail.setId(1);
-			detail.setAmount(100);
-			detail.setCount(2);
-			details.add(detail);
-			Order order = new Order();
-			order.setDetails(details);
-			int result = mapper.saveOrderDetail_1(order);
-			assertEquals(1, result);
-		}
-		finally
-		{
-			sqlSession.close();
-		}
-	}
+  @Test
+  public void testSaveOrderDetail_1() {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    try {
+      OrderDao mapper = sqlSession.getMapper(OrderDao.class);
+      List<OrderDetail> details = new ArrayList<OrderDetail>();
+      OrderDetail detail = new OrderDetail();
+      detail.setId(1);
+      detail.setAmount(100);
+      detail.setCount(2);
+      details.add(detail);
+      Order order = new Order();
+      order.setDetails(details);
+      int result = mapper.saveOrderDetail_1(order);
+      assertEquals(1, result);
+    } finally {
+      sqlSession.close();
+    }
+  }
 
-	@Test
-	public void testSaveOrderDetail_2()
-	{
-		SqlSession sqlSession = sqlSessionFactory.openSession();
-		try
-		{
-			OrderDao mapper = sqlSession.getMapper(OrderDao.class);
-			List<OrderDetail> details = new ArrayList<OrderDetail>();
-			OrderDetail detail = new OrderDetail();
-			detail.setId(1);
-			detail.setAmount(100);
-			detail.setCount(2);
-			details.add(detail);
-			int result = mapper.saveOrderDetail_2(details);
-			assertEquals(1, result);
-		}
-		finally
-		{
-			sqlSession.close();
-		}
-	}
+  @Test
+  public void testSaveOrderDetail_2() {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    try {
+      OrderDao mapper = sqlSession.getMapper(OrderDao.class);
+      List<OrderDetail> details = new ArrayList<OrderDetail>();
+      OrderDetail detail = new OrderDetail();
+      detail.setId(1);
+      detail.setAmount(100);
+      detail.setCount(2);
+      details.add(detail);
+      int result = mapper.saveOrderDetail_2(details);
+      assertEquals(1, result);
+    } finally {
+      sqlSession.close();
+    }
+  }
 
-	@Test
-	public void testSaveOrderDetail_3()
-	{
-		SqlSession sqlSession = sqlSessionFactory.openSession();
-		try
-		{
-			OrderDao mapper = sqlSession.getMapper(OrderDao.class);
-			List<OrderDetail> details = new ArrayList<OrderDetail>();
-			OrderDetail detail = new OrderDetail();
-			detail.setId(1);
-			detail.setAmount(100);
-			detail.setCount(2);
-			details.add(detail);
-			int result = mapper.saveOrderDetail_3(details);
-			assertEquals(1, result);
-		}
-		finally
-		{
-			sqlSession.close();
-		}
-	}
+  @Test
+  public void testSaveOrderDetail_3() {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    try {
+      OrderDao mapper = sqlSession.getMapper(OrderDao.class);
+      List<OrderDetail> details = new ArrayList<OrderDetail>();
+      OrderDetail detail = new OrderDetail();
+      detail.setId(1);
+      detail.setAmount(100);
+      detail.setCount(2);
+      details.add(detail);
+      int result = mapper.saveOrderDetail_3(details);
+      assertEquals(1, result);
+    } finally {
+      sqlSession.close();
+    }
+  }
 }
