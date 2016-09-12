b47dda8fc85b0dee20c39ec50ad13e23aae615a1
Iwao AVE! <harawata@gmail.com>
2016-09-13 01:19:35 +0900

    Test case for https://github.com/mybatis/mybatis-3/issues/749

---
+++ gh-749/src/test/java/test/Role.java
@@ -0,0 +1,22 @@
+package test;
+
+public class Role {
+  private Integer id;
+  private String role;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+}
