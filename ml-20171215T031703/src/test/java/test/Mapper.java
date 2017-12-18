4dd754333b2de5f54597c268fd437a0a401005d6
Iwao AVE! <harawata@gmail.com>
2017-12-18 21:21:17 +0900

    Added foreach element

--- ml-20171215T031703/src/test/java/test/Mapper.java
+++ ml-20171215T031703/src/test/java/test/Mapper.java
@@ -14,6 +14,6 @@ import java.util.List;
 
 public interface Mapper {
 
-  List<PoolDoOutDetailModel> getOutDetailRecords();
+  List<PoolDoOutDetailModel> getOutDetailRecords(List<String> params);
 
 }
