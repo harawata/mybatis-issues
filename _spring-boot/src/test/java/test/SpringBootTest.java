2df4ff2dbeecb62916f0d4e27660d65882883c69
Kazuki Shimizu <kazuki43zoo@gmail.com>
2018-11-11 01:32:11 +0900

    Update dependency artifact version

--- _spring-boot/src/test/java/test/SpringBootTest.java
+++ _spring-boot/src/test/java/test/SpringBootTest.java
@@ -19,13 +19,12 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import mapper.UserMapper;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(Application.class)
+@RunWith(SpringRunner.class)
+@org.springframework.boot.test.context.SpringBootTest(classes = Application.class)
 public class SpringBootTest {
 
   @Autowired
