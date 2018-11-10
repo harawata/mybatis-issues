e817ca3c4ac159eadd8ad0073f52336fc1e6a624
Kazuki Shimizu <kazuki43zoo@gmail.com>
2018-11-11 02:22:13 +0900

    Polishing the template of spring-boot

--- _spring-boot/src/main/resources/data.sql
+++ _spring-boot/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 --
---    Copyright 2009-2016 the original author or authors.
+--    Copyright 2009-2018 the original author or authors.
 --
 --    Licensed under the Apache License, Version 2.0 (the "License");
 --    you may not use this file except in compliance with the License.
@@ -13,13 +13,6 @@
 --    See the License for the specific language governing permissions and
 --    limitations under the License.
 --
-
-drop table users if exists;
-
-create table users (
-  id int,
-  name varchar(20)
-);
 
 insert into users (id, name) values(1, 'User1');
 insert into users (id, name) values(2, 'User2');
