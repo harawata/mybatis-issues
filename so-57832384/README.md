d8771031d533b0311c79dab26645c1b8e1b6892d
Iwao AVE! <harawata@gmail.com>
2019-09-07 21:07:20 +0900

    Demo iterating map property of a bean
    https://stackoverflow.com/questions/57832384

---
+++ so-57832384/README.md
@@ -0,0 +1,5 @@
+```sh
+$ svn export https://github.com/harawata/mybatis-issues/trunk/so-57832384
+$ cd so-57832384
+$ mvn test
+```
---
+++ so-57832384/src/test/java/test/BeanA.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.util.Map;
+
+public class BeanA {
+  private Map<String, String> map;
+
+  public Map<String, String> getMap() {
+    return map;
+  }
+
+  public void setMap(Map<String, String> map) {
+    this.map = map;
+  }
+}
