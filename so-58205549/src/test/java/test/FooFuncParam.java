c776ee75f8a52e85e9b698514f701b6159079e14
Iwao AVE! <harawata@gmail.com>
2019-10-03 23:32:06 +0900

    Retrieve boolean via OUT param of Oracle FUNCTION
    https://stackoverflow.com/questions/58205549

---
+++ so-58205549/src/test/java/test/FooFuncParam.java
@@ -0,0 +1,40 @@
+package test;
+
+public class FooFuncParam {
+  private Integer result;
+  private String foo;
+  private boolean fooOutput;
+  private String error;
+
+  public Integer getResult() {
+    return result;
+  }
+
+  public void setResult(Integer result) {
+    this.result = result;
+  }
+
+  public String getFoo() {
+    return foo;
+  }
+
+  public void setFoo(String foo) {
+    this.foo = foo;
+  }
+
+  public boolean isFooOutput() {
+    return fooOutput;
+  }
+
+  public void setFooOutput(boolean fooOutput) {
+    this.fooOutput = fooOutput;
+  }
+
+  public String getError() {
+    return error;
+  }
+
+  public void setError(String error) {
+    this.error = error;
+  }
+}
