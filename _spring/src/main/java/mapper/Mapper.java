5d86dbb197b3cfab69b38c89442df2da8c46e001
Iwao AVE! <harawata@gmail.com>
2022-12-07 03:36:28 +0900

    [maint] Spring 6, JUnit 5

--- _spring/src/main/java/mapper/Mapper.java
+++ _spring/src/main/java/mapper/Mapper.java
@@ -5,5 +5,5 @@ import test.User;
 public interface Mapper {
   User getUser(Integer id);
 
-  void insertUser(User user);
+  int insertUser(User user);
 }
--- _spring/src/test/java/test/SpringTest.java
+++ _spring/src/test/java/test/SpringTest.java
@@ -1,34 +1,35 @@
 package test;
 
-import static org.junit.Assert.*;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import mapper.Mapper;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
-public class SpringTest {
+class SpringTest {
 
   @Autowired
   private Mapper mapper;
 
   @Test
-  public void shouldGetAUser() {
+  void shouldGetAUser() {
     User user = mapper.getUser(1);
     assertEquals("User1", user.getName());
   }
 
   @Test
-  public void shouldInsertAUser() {
+  void shouldInsertAUser() {
     User user = new User();
     user.setId(2);
     user.setName("User2");
-    mapper.insertUser(user);
+    assertEquals(1, mapper.insertUser(user));
   }
 
 }
