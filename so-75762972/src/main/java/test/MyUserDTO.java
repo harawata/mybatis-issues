87132a5fccaa37f648d101073460107d04a21fb7
Iwao AVE! <harawata@gmail.com>
2023-03-21 07:33:51 +0900

    Add `final` to all fields
    The test still passes.
    
    To reproduce the issue you described, there needs to be some kind of anomaly like...
    
    - There is mismatch between argument order and column order in the result set.
    - Not all fields are `final` which also causes mismatch between constructor arguments and columns.

--- so-75762972/src/main/java/test/MyUserDTO.java
+++ so-75762972/src/main/java/test/MyUserDTO.java
@@ -4,8 +4,8 @@ import lombok.Data;
 
 @Data
 public class MyUserDTO {
-  private Long id;
-  private String name;
-  private Integer age;
-  private String address;
+  private final Long id;
+  private final String name;
+  private final Integer age;
+  private final String address;
 }
